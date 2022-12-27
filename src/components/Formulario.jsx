6916cdb87ab82d9faf931fd72943f23a3d795abf
Ivan Caceres <dg2.ivancaceres@gmail.com>
2022-12-27 00:03:06 -0300

    Se instalo tailwind y se crearon los primeros estilos

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    <div>
+        <h1>SOY FORMULARIO</h1>
+    </div>
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div>
+        <h1>Listado Pacientes</h1>
+    </div>
+  )
+}
+
+export default ListadoPacientes
